e183dde0d5a351bcb77d7e84106b8676273cdf27
Jake Silva <jake.miles.silva@gmail.com>
2016-10-13 23:10:24 -0400

    updated

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
 import logging.config
+import settings
 
 from flask import Flask, Blueprint
-import settings
+from flask_restful import Api
+
 from api.todo.endpoints import Todo, TodoList
 from api.user.endpoints import User, UserList
 from api.upload.endpoints import UploadList
-from api.restful import api,log
+
 from database import db
 
 app = Flask(__name__)
-logging.config.fileConfig('logging.conf')
-log = logging.getLogger(__name__)
 
+app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
+app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
+app.config['UPLOAD_FOLDER'] = settings.UPLOAD_FOLDER
+app.config['ALLOWED_EXTENSIONS'] = settings.ALLOWED_EXTENSIONS
 
+blueprint = Blueprint('api', __name__, url_prefix='/api')
+api = Api(blueprint)
+api.add_resource(TodoList, '/todos')
+app.register_blueprint(blueprint)
+
+db.app = app
+db.init_app(app)
+db.drop_all()
+db.create_all()
+
+#logging.config.fileConfig('logging.conf')
+#log = logging.getLogger(__name__)
+
+"""
 def configure_app(flask_app):
     flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
@@ -37,11 +56,12 @@ def initialize_app(flask_app):
     db.init_app(flask_app)
     db.drop_all()
     db.create_all()
-
+"""
+"""
 def main():
     initialize_app(app)
     log.info('>>>>> Starting development server at http://{}/api/ <<<<<'.format(app.config['SERVER_NAME']))
     app.run(debug=settings.FLASK_DEBUG)
-
+"""
 if __name__ == "__main__":
-    main()
+    app.run(debug=settings.FLASK_DEBUG)
