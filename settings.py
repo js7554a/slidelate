00cb5d339b94206e7b8f43064e69b74fcd115538
Jake Silva <jake.miles.silva@gmail.com>
2016-10-13 20:13:56 -0400

    added is_prod

--- settings.py
+++ settings.py
@@ -1,9 +1,17 @@
+import os
+
+#prod or local
+is_prod = os.environ.get('IS_HEROKU', None)
+
 # Flask settings
 FLASK_SERVER_NAME = 'localhost:5000'
 FLASK_DEBUG = True  # Do not use debug mode in production
 
 # SQLAlchemy settings
-SQLALCHEMY_DATABASE_URI = 'postgresql://jsilva:jx3t2016@localhost/slidelate'
+if is_prod:
+	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+else:
+	SQLALCHEMY_DATABASE_URI = 'postgresql://jsilva:jx3t2016@localhost/slidelate'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Files
